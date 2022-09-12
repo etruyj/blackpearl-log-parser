26867e89ab9ecc6f94ad45c049abc6a83d144d1d
etruyj <etruyj@gmail.com>
2022-09-12 08:01:11 -0400

    Fixed storage domain display formatting for tables.

--- code/ui/display/serializers/SerializeStorageDomains.java
+++ code/ui/display/serializers/SerializeStorageDomains.java
@@ -18,6 +18,13 @@ public class SerializeStorageDomains
 		ArrayList<OutputFormat> output = new ArrayList<OutputFormat>();
 		OutputFormat formatted_line;
 
+		// These variables are for table formatting. The maximum number
+		// of domain members needs to be accounted for in order to accurately
+		// display the table information.
+		// indent is set to -1 to skip for non-table format.
+		int members = maxMemberCount(domain_list);
+		int counter;
+
 		for(int i=0; i< domain_list.size(); i++)
 		{
 			formatted_line = new OutputFormat();
@@ -28,6 +35,8 @@ public class SerializeStorageDomains
 			formatted_line.key = "storage_domain>write_optimization";
 			formatted_line.value = domain_list.get(i).writeOptimization();
 			output.add(formatted_line);
+
+			counter = 0;
 
 			for(int j=0; j < domain_list.get(i).memberCount(); j++)
 			{
@@ -51,9 +60,60 @@ public class SerializeStorageDomains
 				formatted_line.key = "storage_domain>storage_domain_member>write_optimization";
 				formatted_line.value = domain_list.get(i).memberWritePreference(j);
 				output.add(formatted_line);
+
+				counter++;
+			}
+
+			// Add blank values if necessary to square out the table.
+			for(int j=counter; j<members; j++)
+			{
+				formatted_line = new OutputFormat();
+				formatted_line.key = "storage_domain>storage_domain_member>type";
+				formatted_line.value = "";
+				formatted_line.indents = -1;
+				output.add(formatted_line);
+				formatted_line = new OutputFormat();
+				formatted_line.key = "storage_domain>storage_domain_member>partition";
+				formatted_line.value = "";
+				output.add(formatted_line);
+				formatted_line.indents = -1;
+				formatted_line = new OutputFormat();
+				formatted_line.key = "storage_domain>storage_domain_member>tape_type";
+				formatted_line.value = "";
+				formatted_line.indents = -1;
+				output.add(formatted_line);
+				formatted_line = new OutputFormat();
+				formatted_line.key = "storage_domain>storage_domain_member>state";
+				formatted_line.value = "";
+				formatted_line.indents = -1;
+				output.add(formatted_line);
+				formatted_line = new OutputFormat();
+				formatted_line.key = "storage_domain>storage_domain_member>write_optimization";
+				formatted_line.value = "";
+				formatted_line.indents = -1;
+				output.add(formatted_line);
 			}
 		}
 
 		return output;
+	}
+
+	//=======================================
+	// Private Functions
+	//=======================================
+	
+	private static int maxMemberCount(ArrayList<StorageDomain> domain_list)
+	{
+		int memberCount = 0;
+
+		for(int i=0; i < domain_list.size(); i++)
+		{
+			if(domain_list.get(i).memberCount() < memberCount)
+			{
+				memberCount = domain_list.get(i).memberCount();
+			}
+		}
+
+		return memberCount;
 	}
 }
