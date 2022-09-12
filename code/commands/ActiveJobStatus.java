1bf420929e1bc94d620f8e78ec6f9d02666e0e6d
etruyj <etruyj@gmail.com>
2022-09-12 08:49:16 -0400

    Fixed an issue with ActiveJobStatus

--- code/commands/ActiveJobStatus.java
+++ code/commands/ActiveJobStatus.java
@@ -13,8 +13,10 @@ import com.socialvagrancy.blackpearl.logs.structures.operations.TapeOperation;
 import com.socialvagrancy.blackpearl.logs.structures.outputs.ActiveJob;
 import com.socialvagrancy.blackpearl.logs.structures.outputs.Bucket;
 import com.socialvagrancy.blackpearl.logs.structures.outputs.JobDetails;
+import com.socialvagrancy.blackpearl.logs.structures.rest.GuiTapeLibraryPartitions;
 import com.socialvagrancy.blackpearl.logs.utils.importers.GetCompletedJobs;
 import com.socialvagrancy.blackpearl.logs.utils.importers.GetJobIDtoChunkMap;
+import com.socialvagrancy.blackpearl.logs.utils.importers.rest.GetTapePartitions;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.GenerateActiveJobs;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.GenerateDS3Operations;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.GeneratePoolOperations;
@@ -22,6 +24,7 @@ import com.socialvagrancy.blackpearl.logs.utils.linkers.GenerateTapeOperations;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.LinkJobToDS3Operations;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.LinkJobToPoolOperations;
 import com.socialvagrancy.blackpearl.logs.utils.linkers.LinkJobToTapeOperations;
+import com.socialvagrancy.blackpearl.logs.utils.linkers.MapBucketToID;
 import com.socialvagrancy.utils.Logger;
 
 import java.util.ArrayList;
@@ -34,15 +37,21 @@ public class ActiveJobStatus
 		boolean debugging = false;
 		Logger log = new Logger("../logs/bp_logs.log", 102400, 5, 1);
 		CompletedJob jobs = GetCompletedJobs.fromJson(dir_path + "/rest/gui_ds3_active_jobs.json");
-		ArrayList<TapeOperation> ops_list = GenerateTapeOperations.fromLogs(dir_path, 8, 14, log, debugging);
+		GuiTapeLibraryPartitions tape_partitions = GetTapePartitions.fromRest(dir_path);
+		ArrayList<Bucket> bucket_list = ListBuckets.fromRest(dir_path);
+		
+		
+		ArrayList<TapeOperation> ops_list = GenerateTapeOperations.fromLogs(dir_path, 8, 14, tape_partitions, log, debugging);
 		ArrayList<PoolOperation> pool_ops_list = GeneratePoolOperations.fromLogs(dir_path, 8, log, debugging);
 		ArrayList<DS3Operation> ds3_ops_list = GenerateDS3Operations.fromLogs(dir_path, 8, log, debugging);
+		
 		HashMap<String, ArrayList<String>> id_chunk_map = GetJobIDtoChunkMap.fromDataplanner(dir_path, 8, log, debugging);
-		ArrayList<JobDetails> details_list = LinkJobToTapeOperations.createDetails(jobs, id_chunk_map, ops_list, log);
+		HashMap<String, Bucket> id_bucket_map = MapBucketToID.createMap(bucket_list);
+		
+		ArrayList<JobDetails> details_list = LinkJobToTapeOperations.createDetails(jobs, id_chunk_map, id_bucket_map, ops_list, log);
 		details_list = LinkJobToPoolOperations.addPools(details_list, id_chunk_map, pool_ops_list, log);
 		details_list = LinkJobToDS3Operations.addDS3(details_list, id_chunk_map, ds3_ops_list, log);
 
-		ArrayList<Bucket> bucket_list = ListBuckets.fromRest(dir_path);
 		
 		ArrayList<ActiveJob> job_list = GenerateActiveJobs.fromLogset(details_list, bucket_list, id_chunk_map);
 
