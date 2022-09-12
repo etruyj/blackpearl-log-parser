202538c47878354c706c2b7e8ba91bf69f39fddb
etruyj <etruyj@gmail.com>
2022-09-12 17:26:47 -0400

    Initial variable declaration for the fetch-configuration command.

---
+++ code/structures/outputs/CIFSShare.java
@@ -0,0 +1,12 @@
+//===================================================================
+// CIFSShare.java
+// 	Description:
+// 		Holds CIFS share specific information
+//===================================================================
+
+package com.socialvagrancy.blackpearl.logs.structures.outputs;
+
+public class CIFSShare extends Share
+{
+	public boolean read_only;
+}
---
+++ code/structures/outputs/Share.java
@@ -0,0 +1,17 @@
+//===================================================================
+// Share.java
+// 	Description:
+// 		Parent class for CIFS and NFS shares.
+// 		Also works as the Vail share class as it holds
+// 		the same information.
+//===================================================================
+
+package com.socialvagrancy.blackpearl.logs.structures.outputs;
+
+public class Share
+{
+	public String name;
+	public String volume_id;
+	public String volume_name;
+	public String mount_point;
+}
